03930b95cc00fa0fa6e77610d3c6f5d996b7f1a2
Vadim Avkhimenia <avkhimen@ualberta.ca>
2024-01-09 05:47:35 +0000

    Adding functionality to determine MRN

--- read_image.py
+++ read_image.py
@@ -2,7 +2,7 @@ import easyocr
 import re
 import math
 
-def countDigit(n): 
+def count_digit(n): 
     return math.floor(math.log10(n)+1) 
 
 reader = easyocr.Reader(['en'])
@@ -20,16 +20,22 @@ def get_info_from_img(arr):
     DOB = None
     first_name = None
     last_name = None
+    MRN = None
     for a in arr:
         a = a[1].split()
         for elem in a:
             try:
                 elem = re.sub(r"[^a-zA-Z]+", "", elem)
                 elem = elem.lower()
+                print(elem)
                 # Gender test
                 if elem == 'male' or elem == 'female':
                     gender = elem.lower()
+                if count_digit(int(elem)) == 10:
+                    MRN = int(elem)
             except Exception as e:
                 continue
+
+    print(gender, DOB, first_name, last_name, MRN)
 
 print(get_info_from_img(result))
